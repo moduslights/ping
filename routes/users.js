8cb7632558d92144a07ba71a4bd13af9989ba6be
Babatunde Alaraje <support@moduslights.com>
2019-05-31 04:31:36 -0500

    add loging to users route

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+const winston = require("winston");
 
 router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
@@ -15,6 +16,7 @@ router.get("/me", auth, async (req, res) => {
 
 router.get("/", async (req, res) => {
   const users = await User.find().select("-password");
+  winston.info("getting all users ....");
   res.send(users);
 });
 
